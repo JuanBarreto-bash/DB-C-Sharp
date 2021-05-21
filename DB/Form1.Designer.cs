f4a2c585ee1e5ba5f31f0769f7452ac345ec7bd8
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2021-05-20 21:29:44 -0300

    Insert y select funcionando

--- DB/Form1.Designer.cs
+++ DB/Form1.Designer.cs
@@ -24,13 +24,152 @@ namespace DB {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgLista = new System.Windows.Forms.DataGridView();
+            this.btnListar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(107, 25);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(107, 53);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 22);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(107, 81);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 22);
+            this.txtEdad.TabIndex = 2;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(107, 109);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Edad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(107, 147);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 23);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgLista
+            // 
+            this.dgLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLista.Location = new System.Drawing.Point(310, 20);
+            this.dgLista.Name = "dgLista";
+            this.dgLista.RowHeadersWidth = 51;
+            this.dgLista.RowTemplate.Height = 24;
+            this.dgLista.Size = new System.Drawing.Size(431, 302);
+            this.dgLista.TabIndex = 9;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(666, 328);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 10;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(982, 547);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.dgLista);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgLista;
+        private System.Windows.Forms.Button btnListar;
     }
 }
 
--- DB/Form1.cs
+++ DB/Form1.cs
@@ -7,11 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DB {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e) {
+            MySqlConnection conexion = new MySqlConnection(
+                "server=192.168.1.44;" +
+                "userid=root;" +
+                "password=1234;" +
+                "database=basesita"
+            );
+
+            conexion.Open();
+            
+            MySqlCommand comando = new MySqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = "INSERT INTO persona(nombre,apellido,edad,email) VALUES(@nombre,@apellido,@edad,@email)";
+
+            comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+            comando.Parameters.AddWithValue("@apellido", txtApellido.Text);
+            comando.Parameters.AddWithValue("@edad", txtEdad.Text);
+            comando.Parameters.AddWithValue("@email", txtEmail.Text);
+
+            comando.Prepare();
+
+
+            comando.ExecuteNonQuery();
+
+            conexion.Close();
+
+            MessageBox.Show("Quedo pronto");
+            ListarDesdeDB();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e) {
+            ListarDesdeDB();
+
+        }
+
+        private void ListarDesdeDB() {
+
+            MySqlConnection conexion = new MySqlConnection(
+                           "server=192.168.1.44;" +
+                           "userid=root;" +
+                           "password=1234;" +
+                           "database=basesita"
+                       );
+
+            conexion.Open();
+
+            MySqlDataReader lector;
+
+            MySqlCommand comando = new MySqlCommand();
+            comando.Connection = conexion;
+            comando.CommandText = "SELECT * FROM persona WHERE id > @numero";
+            comando.Parameters.AddWithValue("@numero", 0);
+            comando.Prepare();
+
+
+            lector = comando.ExecuteReader();
+
+            DataTable tabla = new DataTable();
+            tabla.Load(lector);
+
+            dgLista.DataSource = tabla;
+
+            conexion.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            ListarDesdeDB();
         }
     }
 }
